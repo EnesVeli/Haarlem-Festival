26d5535b29a17a16fd882d19b53ac1ae50042f83
elcoleta <723295@student.inholland.nl>
2026-03-22 16:59:08 +0100

    fixed jazz cms

--- app/src/ViewModels/Jazz/JazzCmsViewModels/JazzLocationsCmsViewModel.php
+++ app/src/ViewModels/Jazz/JazzCmsViewModels/JazzLocationsCmsViewModel.php
@@ -5,12 +5,14 @@ namespace App\ViewModels\Jazz\JazzCmsViewModels;
 class JazzLocationsCmsViewModel
 {
     public array $locations;
+    public array $location;
     public ?array $currentUser;
     public string $activeTab;
 
-    public function __construct(array $locations, ?array $currentUser)
+    public function __construct(array $locations = [], ?array $currentUser = null, array $location = [])
     {
         $this->locations = $locations;
+        $this->location = $location;
         $this->currentUser = $currentUser;
         $this->activeTab = 'locations';
     }
